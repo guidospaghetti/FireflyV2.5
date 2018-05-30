4d6d31f461853e316eae7fb1cf567e29787c7017
Aaron Guidarelli <guidarela2@students.rowan.edu>
2018-05-30 09:28:54 -0400

    Initial stages of wait_for_launch, will serve as basis for all other states of flight

--- flight.c
+++ flight.c
@@ -6,13 +6,20 @@
 #include "collection.h"
 #include "transmission.h"
 
+#define ACCEL_HIST_SIZE	15
+#define ALT_HIST_SIZE	15
+
 void wait_for_launch(void);
 void upwards(void);
 void downwards(void);
 void landed(void);
 
 float accelHist[15] = {0};
-float altHist[10] = {0};
+float altHist[15] = {0};
+float* accelPos = accelHist;
+float* altPos = altHist;
+float* accelEnd = accelHist + ACCEL_HIST_SIZE;
+float* altEnd = accelHist + ALT_HIST_SIZE;
 
 void run_flight(void) {
 	wait_for_launch();
@@ -23,6 +30,27 @@ void run_flight(void) {
 
 void wait_for_launch() {
 	// Get full data collection but use LPM of accelerometer
+	collectionConfig_t config;
+	// Set the LPM of the MPU6050 to 40 Hz
+	config.lpm = 4;
+	config.rate = 40;
+	setup_collection(&config);
+	collection_t data;
+	while (1) {
+		collect(&data);
+		// Assume the payload will be placed flat, GPS down
+		// Z axis will be facing upwards, towards the sky
+		*accelPos = data.data.accel.z;
+		*altPos = data.data.altitude;
+		accelPos++;
+		altPos++;
+		if (accelPos == accelEnd) {
+			accelPos = accelHist;
+		}
+		if (altPos == altEnd) {
+			altPos = altHist;
+		}
+	}
 }
 
 void upwards(void) {
