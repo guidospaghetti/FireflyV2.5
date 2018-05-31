dfe55076f9c3a7bfd34ede2f95e3ea4b18e5e2eb
Aaron Guidarelli <guidarela2@students.rowan.edu>
2018-05-31 18:27:27 -0400

    Add a flight state enumeration

--- FlightLogic/flightTiming.h
+++ FlightLogic/flightTiming.h
@@ -1,5 +1,17 @@
 #include <stdint.h>
 #include "collection.h"
 
+#ifndef FLIGHTTIMING_H
+#define FLIGHTTIMING_H
+
+typedef enum flightState_t {
+	ON_PAD,
+	UPWARDS,
+	DOWNWARDS,
+	LANDED
+} flightState_t;
+
 void initDataStorage(void);
-uint8_t update(collection_t* data);
+flightState_t update(collection_t* data);
+
+#endif
